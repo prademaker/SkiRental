0ffa864fa206d57e1924b29ce20d461044683250
Paul Rademaker <paulrademaker@Pauls-MacBook-Pro.local>
2026-03-23 14:53:36 +0100

    first commit

---
+++ src/main/java/nl/miwnn/ch19/paul/skirental/SkirentalApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch19.paul.skirental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkirentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkirentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch19/paul/skirental/SkirentalApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch19.paul.skirental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkirentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
